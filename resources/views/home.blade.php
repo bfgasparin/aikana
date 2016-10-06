e04d0a1da2408f9057ab43cc8695551ca0eb4817
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2016-10-06 02:49:08 -0300

    Changed home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,16 @@
 <<section class="section">
     <div class="container">
       <div class="heading">
-        <h1 class="title">Aeeeee</h1>
+        <h1 class="title">Pronto!</h1>
         <h2 class="subtitle">
-          Os serviços desse aplicativo ainda não estão liberados para uso. Serão liberados
+          Você já está no {{ config('app.name') }}.
+        </h2>
+        <h2 class="subtitle">
+          Mas, os serviços do {{ config('app.name') }} ainda não estão liberados para uso. Serão liberados
           apenas no dia da festa. <strong>Ate lá</strong>
+        </h2>
+        <h2 class="subtitle">
+          <strong>Ate lá</strong>
         </h2>
       </div>
     </div>
