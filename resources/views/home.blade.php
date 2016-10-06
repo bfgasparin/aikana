0a79b218698802413af4dbec761883813a941868
Bruno Ferme Gasparin <bfgasparin@gmail.com>
2016-10-06 02:46:32 -0300

    Created home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<<section class="section">
+    <div class="container">
+      <div class="heading">
+        <h1 class="title">Aeeeee</h1>
+        <h2 class="subtitle">
+          Os serviços desse aplicativo ainda não estão liberados para uso. Serão liberados
+          apenas no dia da festa. <strong>Ate lá</strong>
+        </h2>
+      </div>
     </div>
-</div>
+  </section>
 @endsection
